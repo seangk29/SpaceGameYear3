dfdd43f297b2877601724a2cee2f03e39bcc0580
jayaanderson005 <jay.a.anderson.005@gmail.com>
2026-04-02 00:09:37 +0100

    shop works
    now just need to figure out how to implement into hub scene

---
+++ Assets/Scripts/UI/ShopButtons.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ShopButtons : MonoBehaviour
+{
+    public GameObject background;
+    public TextMeshProUGUI conTxt;
+    public TextMeshProUGUI upgTxt;
+
+    public ShopText[] shopText;
+    public int indic;
+
+    public PermaPlayerStats PermaPlayerStats;
+
+    private void Start()
+    {
+        PermaPlayerStats = GameObject.Find("PlayerData").GetComponent<PermaPlayerStats>();
+    }
+
+
+    public void healthUpgrade()
+    {
+       background.SetActive(true);
+        conTxt.text = shopText[0].conTxt;
+        upgTxt.text = shopText[0].upgradeTxt;
+        indic = 1;
+    }
+
+    public void shieldUpgrade()
+    {
+        background.SetActive(true);
+        conTxt.text = shopText[1].conTxt;
+        upgTxt.text = shopText[1].upgradeTxt;
+        indic = 2;
+    }
+
+    public void shieldCooldown()
+    {
+        background.SetActive(true);
+        conTxt.text = shopText[2].conTxt;
+        upgTxt.text = shopText[2].upgradeTxt;
+        indic = 3;
+    }
+
+    public void bulletUpgrade()
+    {
+        background.SetActive(true);
+        conTxt.text = shopText[3].conTxt;
+        upgTxt.text = shopText[3].upgradeTxt;
+        indic = 4;
+    }
+
+    public void speedUpgrade()
+    {
+        background.SetActive(true);
+        conTxt.text = shopText[4].conTxt;
+        upgTxt.text = shopText[4].upgradeTxt;
+        indic = 5;
+    }
+
+    public void spBulletHpUpgrade()
+    {
+        background.SetActive(true);
+        conTxt.text = shopText[5].conTxt;
+        upgTxt.text = shopText[5].upgradeTxt;
+        indic = 6;
+    }
+
+    public void spBulletCooldownUpgrade()
+    {
+        background.SetActive(true);
+        conTxt.text = shopText[6].conTxt;
+        upgTxt.text = shopText[6].upgradeTxt;
+        indic = 7;
+    }
+
+    public void lifeUpgrade()
+    {
+        background.SetActive(true);
+        conTxt.text = shopText[7].conTxt;
+        upgTxt.text = shopText[7].upgradeTxt;
+        indic = 8;
+    }
+
+    public void confirmUpgrade()
+    {
+
+        switch(indic)
+        {
+            case 1:
+                PermaPlayerStats.maxHealth++;
+                PermaPlayerStats.health++;
+            break;
+            case 2:
+                PermaPlayerStats.maxShield++;
+                PermaPlayerStats.shield++;
+                break;
+            case 3:
+                PermaPlayerStats.regenShieldTimer++;
+                break;
+            case 4:
+                PermaPlayerStats.damage++;
+                break;
+            case 5:
+                PermaPlayerStats.maxSpeed++;
+                PermaPlayerStats.speed++;
+                break;
+            case 6:
+                PermaPlayerStats.spBulletHealth++;
+
+                break;
+            case 7:
+                PermaPlayerStats.maxSpeed++;
+                PermaPlayerStats.speed++;
+                break;
+            case 8:
+                PermaPlayerStats.maxSpeed++;
+                PermaPlayerStats.speed++;
+                break;
+
+
+        }
+
+
+        background.SetActive(false);
+    }
+
+    public void denyUpgrades()
+    {
+        background.SetActive(false);
+    }
+
+}
+
